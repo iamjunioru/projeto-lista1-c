4b70bb76e31a6e006b04d704ec3ccd2a97e43674
Junior <72615418+iamjunioru@users.noreply.github.com>
2021-11-11 02:34:05 -0300

    ah

--- 5 em C.cpp
+++ 5 em C.cpp
@@ -12,17 +12,17 @@ int idade, tempo_servico;
 printf ("*------------------------------------------------------*\n");    
 printf ("|                    POO - LISTA 1                     |\n");
 printf ("*------------------------------------------------------*\n");
-printf("5. Leia a idade e o tempo de serviço de um trabalhador e\nescreva se ele pode ou não se aposentar.\nAs condições para aposentadoria são:\n* Ter pelo menos 65 anos.\n* Ter trabalhado pelo menos 30 anos.\n* Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.\n");
+printf("5. Leia a idade e o tempo de serviÃ§o de um trabalhador e\nescreva se ele pode ou nÃ£o se aposentar.\nAs condiÃ§Ãµes para aposentadoria sÃ£o:\n* Ter pelo menos 65 anos.\n* Ter trabalhado pelo menos 30 anos.\n* Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.\n");
 printf ("*------------------------------------------------------------*\n");
 printf("- Qual a sua idade?\nDigite abaixo:\n");
 scanf("%d", &idade);
-printf("- Você tem quanto tempo de serviço na Junior's Corporation?\nDigite abaixo:\n");
+printf("- VocÃª tem quanto tempo de serviÃ§o na Junior's Corporation?\nDigite abaixo:\n");
 scanf("%d", &tempo_servico);
 system("cls");
 
 if(idade >= 65 || tempo_servico >= 30 || (idade>=60 && tempo_servico >=25)){
 system("color A2");
-printf("\n\n* Muito velho, você pode se aposentar! :D\nPARABÉNS!\n\n");
+printf("\n\n* Muito velho, vocÃª pode se aposentar! :D\nPARABÃ‰NS!\n\n");
 system("color A2");
 system("color 2A");
 system("color A2");
@@ -43,7 +43,7 @@ system("color A2");
 system("color af");
 
 }else {
-printf("* Cê tá novo ainda amigão, ainda não pode ser aposentar.\n");
+printf("* CÃª tÃ¡ novo ainda amigÃ£o, ainda nÃ£o pode ser aposentar.\n");
 printf("VOLTE AO TRABALHO!\n");
 system("color 40");
 for (int i = 0; i < 7; i++){
@@ -61,4 +61,3 @@ for (int i = 0; i < 7; i++){
 }
 return 0;
 }
-
